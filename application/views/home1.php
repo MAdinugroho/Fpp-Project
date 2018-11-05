1833accb44dd656ab508a6808b1911c7f13bc47d
madinugroho <madinugroho@student.ce.undip.ac.id>
2018-11-05 11:11:25 +0700

    update template frontend

--- application/views/home1.php
+++ application/views/home1.php
@@ -14,9 +14,9 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="./assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
+  <link href="<?= base_url('assets'); ?>//assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="./assets/demo/demo.css" rel="stylesheet" />
+  <link href="<?= base_url('assets'); ?>//assets/demo/demo.css" rel="stylesheet" />
 </head>
 
 <body class="index-page sidebar-collapse">
@@ -71,7 +71,7 @@
       </div>
     </div>
   </nav>
-  <div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('./assets/img/bg2.jpg');">
+  <div class="page-header header-filter clear-filter purple-filter" data-parallax="true" style="background-image: url('<?= base_url('assets'); ?>//assets/img/bg2.jpg');">
     <div class="container">
       <div class="row">
         <div class="col-md-8 ml-auto mr-auto">
@@ -1439,18 +1439,18 @@
     </div>
   </footer>
   <!--   Core JS Files   -->
-  <script src="./assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="./assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="./assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-  <script src="./assets/js/plugins/moment.min.js"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/plugins/moment.min.js"></script>
   <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-  <script src="./assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="./assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
   <!--	Plugin for Sharrre btn -->
-  <script src="./assets/js/plugins/jquery.sharrre.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/plugins/jquery.sharrre.js" type="text/javascript"></script>
   <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="./assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>//assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script>
   <script>
     $(document).ready(function() {
       //init DateTimePickers
